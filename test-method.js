9be61f3bda2fafd6170ed46323bffa05e780b974
Jacob Benaim <benaimjacob@gmail.com>
2019-05-04 01:19:11 +0000

    added Where's Waldo example

--- test-method.js
+++ test-method.js
@@ -1,4 +1,9 @@
 let myString = "Hello, World!";
 let myRegex = /Hello/;
-let result = myRegex.test(myString); 
+let result = myRegex.test(myString); //  returns true if myString contains myRegex 
 console.log(result); // Change this line
+
+let waldoIsHiding = "Somewhere waldo is hiding in this text. asdaSdasdasdaASaadGvsdvdcWaldofasdasdasfas";
+let waldoRegex = /Waldo/; // Change this line
+let results = waldoRegex.test(waldoIsHiding);
+console.log(results);
